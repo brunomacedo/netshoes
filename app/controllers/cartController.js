b2bf678b9c8cff2b6b1d35521e125db25dc87907
Bruno Macedo <contato@brunomacedo.com.br>
2016-09-28 22:32:24 -0300

    Upload project

---
+++ app/controllers/cartController.js
@@ -0,0 +1,9 @@
+myApp.controller('CartController', function($scope, $http) {
+  $scope.products = [
+    //JSON...
+  ];
+
+  $scope.remove = function(index) {
+    $scope.products.splice(index, 1);
+  }
+});
---
+++ app/controllers/mainController.js
@@ -0,0 +1,7 @@
+// EXTERNAL JSON //
+myApp.controller('MyController', function($scope, $http) {
+  $http.get('public/data/products.json')
+   .then(function(res){
+      $scope.products = res.data;
+    });
+});
