160568aed7b50c9e9b355a76891b6404d848c6c1
Bruno Macedo <contato@brunomacedo.com.br>
2016-10-03 11:34:17 -0300

    Add product in cart, sucessful

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,9 +1,17 @@
-(function (){
-	// EXTERNAL JSON //
-	myApp.controller('MyController', function($scope, $http) {
-	  $http.get('public/data/products.json')
-	   .then(function(res){
-	      $scope.products = res.data;
-	    });
+// EXTERNAL JSON //
+myApp.controller('MyController', function($scope, $http) {
+  $http.get('public/data/products.json')
+   .then(function(res){
+      $scope.products = res.data.products;
 	});
-}());
+
+   	$scope.cartProducts = [];
+
+	$scope.addCart = function() {
+		$scope.cartProducts.push({
+			//$scope.products = res.data;
+		});
+		console.log("Get animations.");
+	}
+
+});
