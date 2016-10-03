7c34622c71ebd1733c8f50065650a18414a3b5ee
Bruno Macedo <contato@brunomacedo.com.br>
2016-10-03 11:35:04 -0300

    Run gulp and remove some files

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('build-css', function () {
-  return gulp.src('./assets/css/**/*.scss')
+  return gulp.src('./assets/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
  
 //Watch task
 gulp.task('default', function() {
-   gulp.watch('./assets/css/**/*.scss',['build-css']);
+   gulp.watch('./assets/scss/**/*.scss',['build-css']);
 });
